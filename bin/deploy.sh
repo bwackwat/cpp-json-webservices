04a8b44e66637d3f1e0f40507525dae5249eb3f2
root <root@localhost.localdomain>
2016-10-10 01:23:00 -0700

    Nits before deploy.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,7 @@ git pull
 cd ../cpp-json-webservices
 git pull
 
+# THIS WONT WORK UNLESS THE USER EXECUTING THE SCRIPT OWNS THE ITEMS (bwackwat)
 /bin/cp -r ../friendly-adventure/* /etc/nginx/html/
 /bin/cp ./bin/nginx.conf /etc/nginx/
 
